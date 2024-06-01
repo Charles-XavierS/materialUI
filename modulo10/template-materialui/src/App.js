cb1d7173567c11cebbbd446c6870659e9783398c
Charles Xavier Saldanha <94417307+Charles-XavierS@users.noreply.github.com>
2024-06-01 17:36:00 +0000

    style: Criado tema com cores primaria e secundária. Adicionado o tema ao app, mudando header e botões.

--- modulo10/template-materialui/src/App.js
+++ modulo10/template-materialui/src/App.js
@@ -1,12 +1,26 @@
 import Header from "./components/header";
 import ListarTarefa from "./pages/tarefa/ListarTarefa";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#5e35b1',
+    },
+    secondary: {
+      main: '#5e35b1',
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <ListarTarefa />
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Header />
+        <ListarTarefa />
+      </div>
+    </ThemeProvider>
   );
 }
 
